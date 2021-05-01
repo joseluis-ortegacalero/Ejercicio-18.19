c9ebc4c0aefaa871a88ad7384a3afbb7eb5dcb1c
José Luis Ortega Calero <aracenatic@gmail.com>
2021-05-01 06:43:39 +0200

    Prueba

---
+++ Ejercicio-18.19/Lista.h
@@ -0,0 +1,27 @@
+#ifndef Lista_h
+#define Lista_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Defino estructuras NODO y LISTA
+
+typedef struct Nodo{
+    
+    char letra;
+    struct Nodo* siguiente;
+    struct Nodo* anterior;
+       
+}Nodo;
+
+typedef struct Lista{
+
+    Nodo* cabeza;
+    int numelem;
+    
+}Lista;
+
+//Funciones accesibles desde el main.c INTERFAZ
+
+
+#endif /* Lista_h */
--- Ejercicio-18.19/main.c
+++ Ejercicio-18.19/main.c
@@ -1,14 +1,10 @@
-//
-//  main.c
-//  Ejercicio-18.19
-//
-//  Created by JOSE LUIS ORTEGA CALERO on 1/5/21.
-//
-
+/* 18.19. Escribir un programa para obtener una lista doblemente enlazada con los caracteres de una cadena leída desde el teclado. Cada nodo de la lista tendrá un carácter. Una vez que se tiene la lista ordenarla alfabéticamente y escribirla por pantalla.
+ */
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    
+    
     return 0;
 }
