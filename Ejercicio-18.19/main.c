17ccb6c0520f5850c176a53860cc631bcf7a0c78
José Luis Ortega Calero <aracenatic@gmail.com>
2021-05-01 06:23:56 +0200

    Initial Commit

---
+++ Ejercicio-18.19/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Ejercicio-18.19
+//
+//  Created by JOSE LUIS ORTEGA CALERO on 1/5/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
